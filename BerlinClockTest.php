ca7fd818b2e1ea14df7678e6f537868593937a36
Cezary Mnich <cezary.mnich@student.vinci.be>
2020-10-21 17:04:28 +0200

    add - test BerlinClockTest

---
+++ BerlinClockTest.php
@@ -0,0 +1,12 @@
+<?php
+
+require "vendor/autoload.php";
+require "BerlinClock.php";
+
+use BerlinClock\BerlinClock;
+use PHPUnit\Framework\TestCase;
+
+class BerlinClockTest extends TestCase
+{
+
+}
