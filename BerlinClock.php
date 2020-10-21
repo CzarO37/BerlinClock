9ce5de779b1fbbf162c9acb25b4ea419b1e5624a
Cezary Mnich <cezary.mnich@student.vinci.be>
2020-10-21 17:18:59 +0200

    add - convertMinutes for input 0

--- BerlinClock.php
+++ BerlinClock.php
@@ -4,5 +4,7 @@ namespace BerlinClock;
 
 class BerlinClock
 {
-
+    public function convertMinutes(int $int): string {
+        return "OOOO";
+    }
 }
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -8,5 +8,11 @@ use PHPUnit\Framework\TestCase;
 
 class BerlinClockTest extends TestCase
 {
+    public function test_convertMinutes_given0_shouldReturnOOOO() {
+        $berlinClock = new BerlinClock();
 
+        $actual = $berlinClock->convertMinutes(0);
+
+        $this->assertEquals("OOOO",$actual);
+    }
 }
